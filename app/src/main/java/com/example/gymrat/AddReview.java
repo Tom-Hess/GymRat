d3cfc995a80e727c4ebb6264e451ef1531766e89
Tom Hess <tomhess1759@gmail.com>
2019-05-11 09:55:40 -0400

    initial files

---
+++ app/src/main/java/com/example/gymrat/AddReview.java
@@ -0,0 +1,42 @@
+package com.example.gymrat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class AddReview extends AppCompatActivity {
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    Intent addReview = new Intent(AddReview.this, MainActivity.class);
+                    startActivity(addReview);
+                    return true;
+                case R.id.navigation_search:
+                    return true;
+                case R.id.navigation_add_review:
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_review);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
